5791b98b5b9b81e3566233d05614c9a84ae16f4a
Renato Bonfim Jr <32143600+renatobjr@users.noreply.github.com>
2022-02-19 10:39:28 -0300

    Atualização do Seed de Usuarios

--- app/Database/Seeds/Usuario.php
+++ app/Database/Seeds/Usuario.php
@@ -21,74 +21,74 @@ class Usuario extends Seeder
                 'password' => null,
                 'username' => 'alessandro.amorim',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Auta Oliveira',
                 'password' => null,
                 'username' => 'auta.oliveira',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Jessica Oliveira',
                 'password' => null,
                 'username' => 'jessica.oliveira',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Juhliana Nunes',
                 'password' => null,
                 'username' => 'juhliana.nunes',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Karen Jullia',
                 'password' => null,
                 'username' => 'karen.jullia',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Lidiane Correia',
                 'password' => null,
                 'username' => 'lidiane.correia',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Natalia Viana',
                 'password' => null,
                 'username' => 'natalia.viana',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Renan Santos',
                 'password' => null,
                 'username' => 'renan.santos',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Suelen Silva',
                 'password' => null,
                 'username' => 'suelen.silva',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
             [
                 'nome_completo' => 'Thaís Costa',
                 'password' => null,
                 'username' => 'thais.costa',
                 'isAdmin' => '1',
-                'isRegister' => '1'
+                'firstLogin' => '1'
             ],
         ];
 
         // Running query
-        $this->db->table('usuarios')->insert($data);
+        $this->db->table('usuarios')->insertBatch($data);
     }
 }
